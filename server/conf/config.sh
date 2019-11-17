df7e9c83b4ee9ce0f213196d73780553fffeb8be
Guy Cohen <guyc@gettaxi.com>
2019-11-17 12:26:49 +0200

    Add optinal configuration for Kinesis

--- server/conf/config.sh
+++ server/conf/config.sh
@@ -6,7 +6,7 @@ function set_s3_accees_key {
     then
         sed -i -e "s|%AWS_ACCESS_KEY%|${AWS_ACCESS_KEY_ID}|g" -e "s|%AWS_SECRET_KEY%|${AWS_SECRET_ACCESS_KEY}|g" ${file_name}
     else
-        sed -i -e "s|hive.s3.aws-access-key=%AWS_ACCESS_KEY%||g" -e "s|hive.s3.aws-secret-key=%AWS_SECRET_KEY%||g" ${file_name}
+        sed -i -e "/%AWS_ACCESS_KEY%/d" -e "/%AWS_SECRET_KEY%/d" ${file_name}
     fi
 }
 
@@ -24,11 +24,22 @@ fi
 cp /opt/presto/etc/hive.template.properties /opt/presto/etc/catalog/hive.properties
 set_s3_accees_key /opt/presto/etc/catalog/hive.properties
 
-if [[ -n "$ICEBERG_DIR" ]]
+if [[ -n "${ICEBERG_DIR}" ]]
 then
     cp /opt/presto/etc/iceberg.template.properties /opt/presto/etc/catalog/iceberg.properties
     sed -i -e "s|%ICEBERG_DIR%|${ICEBERG_DIR}|g" /opt/presto/etc/catalog/iceberg.properties
     set_s3_accees_key /opt/presto/etc/catalog/iceberg.properties
 else
     rm -f /opt/presto/etc/catalog/iceberg.properties
+fi
+
+if [[ -z "${KINESIS_DIR}" ]] && [[ -d "/opt/presto/etc/kinesis" ]]
+then
+    KINESIS_DIR="/opt/presto/etc/kinesis"
+fi
+if [[ -n "${KINESIS_DIR}" ]]
+then
+    cp /opt/presto/etc/kinesis.template.properties /opt/presto/etc/catalog/kinesis.properties
+    sed -i -e "s|%KINESIS_DIR%|${KINESIS_DIR}|g" /opt/presto/etc/catalog/kinesis.properties
+    set_s3_accees_key /opt/presto/etc/catalog/kinesis.properties
 fi
